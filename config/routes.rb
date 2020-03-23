4f96778021c3aa32820376b372def4a1ae308f94
Paulina Blaszkiewicz <paulina.blaszk@gmail.com>
2020-03-23 15:26:08 +0100

    Create controllers and config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users,
+               path: '',
+               path_names: {
+                 sign_in: 'login',
+                 sign_out: 'logout',
+                 registration: 'signup'
+               },
+               controllers: {
+                 sessions: 'sessions',
+                 registrations: 'registrations'
+               }
+
   root 'static#index'
+  
 end
